a48185ac053d6b9e4d88a1166308008da744a514
zsaaupo <zakariasarker407@gmail.com>
2023-03-26 13:17:12 +0600

    image error fix

--- store/models.py
+++ store/models.py
@@ -19,6 +19,15 @@ class Product(models.Model):
     
     def __str__(self):
         return self.name
+    
+    @property
+    def imageURL(self):
+        
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
 
 class Order(models.Model):
     
