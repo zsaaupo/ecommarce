e3abfd56d102d6525e2a0e27fa1ed8ea23bd0f5a
zsaaupo <zakariasarker407@gmail.com>
2023-03-26 12:48:32 +0600

    first commit

---
+++ store/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Customer(models.Model):
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=200)
+    
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    
+    name = models.CharField(max_length=200)
+    price = models.FloatField()
+    digital = models.BooleanField(default=False, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    date_orderd = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False)
+    transaction_id = models.CharField(max_length=200, null=True)
+    
+    def __str__(self):
+        return str(self.id)
+    
+class OrderItem(models.Model):
+    
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    quantitiy = models.IntegerField(default=0)
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+class ShippingAddress(models.Model):
+    
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    address = models.TextField()
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=10)
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.address
---
+++ store/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from .models import *
+
+def store(request):
+    products = Product.objects.all()
+    context = {'products':products}
+    return render(request, 'store/store.html', context)
+
+
+def checkout(request):
+    context = {}
+    return render(request, 'store/checkout.html', context)
+
+
+def cart(request):
+    context = {}
+    return render(request, 'store/cart.html', context)
