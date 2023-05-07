73ce4353c37ccf41684e180a921b7f322b99f36c
zsaaupo <zakariasarker407@gmail.com>
2023-05-07 09:02:30 +0600

    fetch added to js for add to card button

--- static/js/cart.js
+++ static/js/cart.js
@@ -12,7 +12,28 @@ for (i=0; i < updateBtns.length; i++){
             console.log('not logged in')
         }
         else{
-            console.log('User id logged in, sending data...')
+            updateUserOrder(productId, action)
         }
+    })
+}
+
+function updateUserOrder(productId, action){
+    console.log("user is logged in, sending data..")
+    let url = '/update_item/'
+
+    fetch(url, {
+        method: 'POST',
+        headers:{
+            "Content-Type": "application/json"
+        },
+        body:JSON.stringify({"productId": productId, "action": action})
+    })
+
+    .then((response) => {
+        return response.json()
+    })
+    
+    .then((data)=>{
+        console.log('data:', data)
     })
 }
